49247b3d6befdc61c16ea87caf01b094fcdc17e5
mnyk <nykielmat@gmail.com>
2017-09-28 23:55:42 +0200

    collection views and grid layouts

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by mateusz on 28.09.2017.
+//  Copyright © 2017 mateusz. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
